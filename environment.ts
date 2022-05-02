fdea7086350d840309ac0bc145c63e8397f72a3b
David Webb <davidphilipwebb@gmail.com>
2022-05-02 13:01:01 -0300

    Added files

---
+++ environment.ts
@@ -0,0 +1,37 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+    production: false,
+    isTokenEnabled: true,
+    isResellAllowed: false,
+    wallet: 'http://uol.com.br',
+    whitepaper: 'https://pokerfi.finance/docs/whitepaper.pdf',
+    youtube: 'https://youtube.com',
+    instagram: 'https://instagram.com',
+    facebook: 'https://facebook.com',
+    twitter: 'https://twitter.com',
+    cancellation: 'http://uol.com.br',
+    deliveries: 'http://uol.com.br',
+    account: 'http://uol.com.br',
+    exchanges: 'http://uol.com.br',
+    about: 'http://uol.com.br',
+    policy: 'http://uol.com.br',
+    work: 'http://uol.com.br',
+    contact: 'http://uol.com.br',
+    social: 'http://uol.com.br',
+    scripts: {
+        paypal: 'https://www.paypalobjects.com/api/checkout.js',
+        pagseguro: 'https://stc.sandbox.pagseguro.uol.com.br/pagseguro/api/v2/checkout/pagseguro.lightbox.js'
+    }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
